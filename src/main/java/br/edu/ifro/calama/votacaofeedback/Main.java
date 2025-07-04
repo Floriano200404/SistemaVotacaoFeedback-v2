491f4d1c7fe2ad02f55017122dabb2c8d2846dfa
Floriano Vieira <florianoneto2004@gmail.com>
2025-07-04 10:56:29 -0400

    chore: Ajusta arquivos de configuração e utilitários

--- src/main/java/br/edu/ifro/calama/votacaofeedback/Main.java
+++ src/main/java/br/edu/ifro/calama/votacaofeedback/Main.java
@@ -2,15 +2,14 @@ package br.edu.ifro.calama.votacaofeedback;
 
 
 import br.edu.ifro.calama.votacaofeedback.view.LoginView;
-import br.edu.ifro.calama.votacaofeedback.view.MenuPrincipalView;
+
 
 public class Main {
     
     public static void main(String[] args) {
         
         java.awt.EventQueue.invokeLater(() -> {
-//       new LoginView().setVisible(true);
-       new MenuPrincipalView().setVisible(true);
-        });
+   new LoginView().setVisible(true);
+              });
     }
 }
