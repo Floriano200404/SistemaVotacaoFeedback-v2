8047838fbb9c998ae5eb37bcdd5cbcc3169bbc6b
Floriano200404 <106825737+Floriano200404@users.noreply.github.com>
2025-07-05 15:53:14 -0400

    Revert "Feature/perfil usuario"

--- src/main/java/br/edu/ifro/calama/votacaofeedback/util/DatabaseUtil.java
+++ src/main/java/br/edu/ifro/calama/votacaofeedback/util/DatabaseUtil.java
@@ -18,12 +18,8 @@ public class DatabaseUtil {
 <<<<<<< HEAD
         String password = "!Emn172329";
 =======
-<<<<<<< HEAD
-        String password = "!Emn172329";
-=======
         String password = "floripa123";
 >>>>>>> 81bdc61df21ada09ad9e1bdb5e6090f806210324
->>>>>>> 957c8a7f2499e0ff8ac4831932380c10cbd22a76
         return DriverManager.getConnection(url, user, password);
     }
 }
