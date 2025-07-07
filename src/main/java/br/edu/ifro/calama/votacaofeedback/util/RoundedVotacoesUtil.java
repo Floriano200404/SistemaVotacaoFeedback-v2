ba9980a9a088c17a542560db08e96723683046a6
esten <esten@estenio>
2025-07-07 19:15:11 -0400

    style: Estilização dos campos de texto e botões.

---
+++ src/main/java/br/edu/ifro/calama/votacaofeedback/util/RoundedVotacoesUtil.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifro.calama.votacaofeedback.util;
+
+/**
+ *
+ * @author esten
+ */
+
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.RenderingHints;
+import java.awt.Color;
+import javax.swing.border.Border;
+
+public class RoundedVotacoesUtil implements javax.swing.border.Border {
+    private int radius;
+    private Color color;
+
+    public RoundedVotacoesUtil(int radius, Color color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public Insets getBorderInsets(Component c) {
+        return new Insets(this.radius / 2, this.radius, this.radius / 2, this.radius);
+    }
+
+    public boolean isBorderOpaque() {
+        return false; 
+    }
+
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+        Graphics2D g2d = (Graphics2D) g.create();
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setColor(this.color);
+        g2d.drawRoundRect(x, y, width - 1, height - 1, radius, radius);
+        
+        g2d.dispose();
+    }
+}
