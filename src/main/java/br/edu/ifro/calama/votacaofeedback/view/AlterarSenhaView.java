4877f650eafe79b0d9807586d9bdc93ae16b441b
Eychila Vitoria Maia das Chagas <eychilamaia@gmail.com>
2025-07-08 22:58:17 -0400

    feat(view): Adiciona fundo com gradiente na tela de alterar senha

--- src/main/java/br/edu/ifro/calama/votacaofeedback/view/AlterarSenhaView.java
+++ src/main/java/br/edu/ifro/calama/votacaofeedback/view/AlterarSenhaView.java
@@ -4,6 +4,12 @@
  */
 package br.edu.ifro.calama.votacaofeedback.view;
 
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JPanel;
+
 /**
  *
  * @author USER
@@ -17,6 +23,21 @@ public class AlterarSenhaView extends javax.swing.JFrame {
         initComponents();
     }
 
+    class GradientPanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Graphics2D g2d = (Graphics2D) g;
+            Color color1 = new Color(9, 32, 63);
+            Color color2 = new Color(11, 41, 81);
+            int w = getWidth();
+            int h = getHeight();
+            GradientPaint gp = new GradientPaint(0, 0, color1, 0, h, color2);
+            g2d.setPaint(gp);
+            g2d.fillRect(0, 0, w, h);
+        }
+    }
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
